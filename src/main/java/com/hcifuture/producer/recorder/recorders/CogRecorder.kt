f7b3cac022a64e29f75ae03fdc99410e2d4637b3
euxcet <zcc.qwer@gmail.com>
2024-10-29 14:16:41 +0800

    Update the recorder

---
+++ src/main/java/com/hcifuture/producer/recorder/recorders/CogRecorder.kt
@@ -0,0 +1,45 @@
+package com.hcifuture.producer.recorder.recorders
+
+import android.content.Context
+import com.hcifuture.producer.recorder.Collector
+import com.hcifuture.producer.recorder.FileDatasetProvider
+import com.hcifuture.producer.recorder.Recorder
+import com.hcifuture.producer.recorder.UploaderProvider
+import com.hcifuture.producer.recorder.triggers.FixedDurationTrigger
+import com.hcifuture.producer.sensor.NuixSensorManager
+import com.hcifuture.producer.sensor.internal.InternalSensorSpec
+
+class CogRecorder {
+    companion object {
+        fun create(
+            context: Context,
+            nuixSensorManager: NuixSensorManager,
+            fileDatasetProvider: FileDatasetProvider,
+            uploaderProvider: UploaderProvider,
+            datasetName: String,
+        ): Recorder {
+            val fileDataset = fileDatasetProvider.create(datasetName)
+            val uploader = uploaderProvider.create(fileDataset)
+            val collectors: MutableList<Collector> = mutableListOf()
+            nuixSensorManager.internalSensors()
+                .filter {
+                    it.name in listOf(
+                        InternalSensorSpec.accelerometer,
+                        InternalSensorSpec.gyroscope,
+                    )
+                }
+                .onEach {
+                    collectors.addAll(it.defaultCollectors.values)
+                }
+            if (nuixSensorManager.defaultRing.target != null) {
+                collectors.addAll(nuixSensorManager.defaultRing.target!!.defaultCollectors.values)
+            }
+            return Recorder(
+                collectors = collectors,
+                trigger = null,
+                fileDataset = fileDataset,
+                uploader = uploader,
+            )
+        }
+    }
+}
